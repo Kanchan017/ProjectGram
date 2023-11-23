5d9402c3970586290ffd71c549d4efb48409309d
Kanchan017 <130158745+Kanchan017@users.noreply.github.com>
2023-11-24 02:10:12 +0545

    First Version_Punctuation_error

--- Code_01.py
+++ Code_01.py
@@ -21,37 +21,11 @@ def correct_grammar(sentence):
     # Apply corrections to the plain text
     corrected_text = tool.correct(plain_text)
 
-    # Add appropriate punctuation if needed
-    corrected_text = add_punctuation(sentence, corrected_text)
-
     return corrected_text
 
 
-def add_punctuation(original_sentence, corrected_sentence):
-    # Check if the original sentence has punctuation
-    if any(char in '.,;:?!' for char in original_sentence):
-        # Use the original sentence's punctuation
-        return corrected_sentence
-    else:
-        # Check if the corrected sentence ends with appropriate punctuation
-        if not corrected_sentence.endswith(('.', '!', '?')):
-            # Add a period at the end if no punctuation is present
-            corrected_sentence += '.'
-
-        # Ensure the corrected sentence starts with a capital letter
-        corrected_sentence = corrected_sentence.capitalize()
-
-        # Fix possessive forms dynamically
-        words = corrected_sentence.split()
-        for i in range(len(words) - 1):
-            if words[i].endswith('s') and words[i + 1].istitle():
-                words[i] += "'"
-
-        return ' '.join(words)
-
-
-# Example usage with the new sentence
-incorrect_sentence = "he bad withhh englisho for somme reison his namee is alex"
+# Example usage with a new sentence
+incorrect_sentence = "i has a cat"
 corrected_sentence = correct_grammar(incorrect_sentence)
 print("Incorrect:", incorrect_sentence)
 print("Corrected:", corrected_sentence)
